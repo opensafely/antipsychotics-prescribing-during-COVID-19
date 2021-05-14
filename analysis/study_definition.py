8595fa711b55a7ceec9416059ea3716733f82da5
acagreen17 <acagreen17@gmail.com>
2021-05-14 15:45:42 +0100

    Study definition

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,221 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
+######################################
+
+# This script provides the formal specification of the study data that will be extracted from 
+# the OpenSAFELY database.
+
+######################################
 
 
-study = StudyDefinition(
-    default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
-        "rate": "uniform",
-        "incidence": 0.5,
-    },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+# --- IMPORT STATEMENTS ---
+
+## Import code building blocks from cohort extractor package
+from cohortextractor import (
+  StudyDefinition,
+  patients,
+  codelist_from_csv,
+  codelist,
+  filter_codes_by_category,
+  combine_codelists,
 )
+
+## Import codelists from codelist.py (which pulls them from the codelist folder)
+from codelists import *
+  
+  
+# --- DEFINE STUDY POPULATION ---
+  
+## Define study start and end variables explicitly
+start_date = "2019-01-01"
+end_date = "2021-04-30"
+
+## Define study population and variables
+study = StudyDefinition(
+  
+  # Configure the expectations framework
+  default_expectations={
+    "date": {"earliest": "1970-01-01", "latest": end_date},
+    "rate": "uniform",
+    "incidence": 0.2,
+  },
+  
+  # Set index date to start date
+  index_date = start_date,
+  
+  # Define the study population
+  population = patients.satisfying(
+    """
+        NOT has_died
+        AND
+        registered
+        AND
+        age
+        AND
+        has_follow_up_previous_year
+        AND
+        (sex = "M" OR sex = "F")
+        """,
+    
+    has_died = patients.died_from_any_cause(
+      on_or_before="index_date",
+      returning="binary_flag",
+    ),
+    
+    registered = patients.satisfying(
+      "registered_at_start",
+      registered_at_start = patients.registered_as_of(start_date),
+    ),
+    
+    has_follow_up_previous_year = patients.registered_with_one_practice_between(
+      start_date = "index_date - 1 year",
+      end_date = "index_date",
+      return_expectations = {"incidence": 0.95},
+    ),
+    
+  ),
+  
+  
+  ## Groups
+  
+  ### Learning disabilities
+  learning_disability = patients.with_these_clinical_events(
+    learning_disability_codes,
+    returning = "binary_flag",
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
+    return_expectations = {"incidence": 0.2},
+  ),
+  
+  ### Autism
+  autism = patients.with_these_clinical_events(
+    autism_codes,
+    returning = "binary_flag",
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
+    return_expectations = {"incidence": 0.2},
+  ),
+  
+  ### Serious Mental Illness
+  serious_mental_illness = patients.with_these_clinical_events(
+    serious_mental_illness_codes,
+    returning = "binary_flag",
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
+    return_expectations = {"incidence": 0.2},
+  ),
+  
+  ### Care home
+  care_home = patients.with_these_clinical_events(
+    carehome_primis_codes,
+    returning = "binary_flag",
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
+  ),
+  
+  ### Dementia
+  dementia = patients.with_these_clinical_events(
+    dementia_codes,
+    returning = "binary_flag",
+    find_last_match_in_period = True,
+    on_or_before = "index_date",
+  ),
+  
+  
+  ## Variables
+  
+  ### ### Sex
+  sex = patients.sex(
+    return_expectations = {
+      "rate": "universal",
+      "category": {"ratios": {"M": 0.49, "F": 0.51}},
+    }
+  ),
+  
+  ### Ethnicity (Codelists TBC)
+  
+  ### Index of multiple deprivation
+  imd = patients.categorised_as(
+    {"0": "DEFAULT",
+      "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
+      "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+      "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+      "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+      "5": """index_of_multiple_deprivation >= 32844*4/5 """,
+    },
+    index_of_multiple_deprivation = patients.address_as_of(
+      "index_date",
+      returning = "index_of_multiple_deprivation",
+      round_to_nearest = 100,
+    ),
+    return_expectations = {
+      "rate": "universal",
+      "category": {
+        "ratios": {
+          "0": 0.01,
+          "1": 0.20,
+          "2": 0.20,
+          "3": 0.20,
+          "4": 0.20,
+          "5": 0.19,
+        }},
+    },
+  ),
+  
+  ### Age
+  age = patients.age_as_of(
+    "2020-03-31",
+    return_expectations = {
+      "rate": "universal",
+      "int": {"distribution": "population_ages"},
+      "incidence" : 0.001
+    },
+  ),
+  
+  ### Region - NHS England 9 regions
+  region = patients.registered_practice_as_of(
+    "index_date",
+    returning = "nuts1_region_name",
+    return_expectations = {
+      "rate": "universal",
+      "category": {
+        "ratios": {
+          "North East": 0.1,
+          "North West": 0.1,
+          "Yorkshire and The Humber": 0.1,
+          "East Midlands": 0.1,
+          "West Midlands": 0.1,
+          "East": 0.1,
+          "London": 0.2,
+          "South West": 0.1,
+          "South East": 0.1,},},
+    },
+  ),
+  
+  ### STP (regional grouping of practices)
+  stp = patients.registered_practice_as_of("index_date",
+                                           returning = "stp_code",
+                                           return_expectations = {
+                                             "rate": "universal",
+                                             "category": {
+                                               "ratios": {
+                                                 "STP1": 0.1,
+                                                 "STP2": 0.1,
+                                                 "STP3": 0.1,
+                                                 "STP4": 0.1,
+                                                 "STP5": 0.1,
+                                                 "STP6": 0.1,
+                                                 "STP7": 0.1,
+                                                 "STP8": 0.1,
+                                                 "STP9": 0.1,
+                                                 "STP10": 0.1,}},
+                                           },
+  ),
+  
+  ### Depression (Codelists TBC)
+  
+  ### People without a diagnosis of clinical groups above  LD, SMI
+  
+)
+
+
+
+

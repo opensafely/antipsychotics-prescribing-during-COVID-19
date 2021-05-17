2623db404379cab0b1b03ea3fb7bf013666f6d96
acagreen17 <acagreen17@gmail.com>
2021-05-17 14:39:41 +0100

    Add practice

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -157,6 +157,13 @@ study = StudyDefinition(
   
   ## Variables
   
+  ### Practice
+  practice = patients.registered_practice_as_of(
+    "index_date",
+    returning="pseudo_id",
+    return_expectations = {"int" : {"distribution": "normal", "mean": 25, "stddev": 5}, "incidence" : 0.5}
+  ),
+  
   ### Sex
   sex = patients.sex(
     return_expectations = {

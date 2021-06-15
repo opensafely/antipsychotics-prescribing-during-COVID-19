eb8e1da9059e5c20eda8c8232b97417616274439
acagreen17 <acagreen17@gmail.com>
2021-06-15 11:20:16 +0100

    Total number of patients with newly issued antipsychotics plots

--- analysis/summary_plots.R
+++ analysis/summary_plots.R
@@ -20,13 +20,18 @@ dir.create(here::here("output", "figures"), showWarnings = FALSE, recursive=TRUE
 
 ## Import processed data
 data_processed <- readRDS(here::here("output", "data", "data_totals.rds"))
-data_processed_1yr <- readRDS(here::here("output", "data", "data_incident_1y.rds"))
-data_processed_2y <- readRDS(here::here("output", "data", "data_incident_2y.rds"))
+data_processed_1yr <- readRDS(here::here("output", "data", "data_incident_1yr.rds")) %>%
+  mutate(`Number of patients with first prescriptions` = "None in previous year")
+data_processed_2yr <- readRDS(here::here("output", "data", "data_incident_2yr.rds"))  %>%
+  mutate(`Number of patients with first prescriptions` = "None in previous two years")
+
+## Combine data
+data_process_incident <- rbind(data_processed_1yr, data_processed_2yr)
 
 
 # Figures ----
 
-## Total number of antipsychotics issued
+## Total number of patients issued antipsychotics 
 
 ### First generation antipsychotics, excluding long acting depots
 antipsychotics_first_gen <- ggplot(data_processed, aes(x = date, y = antipsychotics_first_gen, colour = group)) +
@@ -88,6 +93,77 @@ prochlorperazine <-  ggplot(data_processed, aes(x = date, y = prochlorperazine, 
   theme(axis.text.x = element_text(angle = 60, hjust = 1))
 
 ggsave(filename=here::here("output", "figures", "plot_total_prochlorperazine.svg"),
+       prochlorperazine,
+       units = "cm", width = 40, height = 20
+)
+
+
+## Total number of patients with newly issued antipsychotics 
+
+### First generation antipsychotics, excluding long acting depots
+antipsychotics_first_gen <- ggplot(data_process_incident, aes(x = date, y = antipsychotics_first_gen, colour = group, linetype = `Number of patients with first prescriptions`)) +
+  geom_line() +
+  facet_wrap(~group, scales = "free") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour=FALSE, linetype = guide_legend(nrow = 2)) +
+  ylab("Total number of patients issued first prescription of \n first generation antipsychotics (excluding long acting depots), per month") +
+  xlab("") +
+  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+ggsave(filename=here::here("output", "figures", "plot_first_antipsychotics_first_gen.svg"),
+       antipsychotics_first_gen,
+       units = "cm", width = 40, height = 20
+)
+
+### Second generation antipsychotics, excluding long acting depots
+antipsychotics_second_gen <-  ggplot(data_processed, aes(x = date, y = antipsychotics_second_gen, colour = group, linetype = `Number of patients with first prescriptions`)) +
+  geom_line() +
+  facet_wrap(~group, scales = "free") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour=FALSE, linetype = guide_legend(nrow = 2)) +
+  ylab("Total number of patients issued first prescription of \n second generation antipsychotics (excluding long acting depots), per month") +
+  xlab("") +
+  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+ggsave(filename=here::here("output", "figures", "plot_first_antipsychotics_second_gen.svg"),
+       antipsychotics_second_gen,
+       units = "cm", width = 40, height = 20
+)
+
+### Long acting injectable and depot antipsychotics
+antipsychotics_injectable_and_depot <-  ggplot(data_processed, aes(x = date, y = antipsychotics_injectable_and_depot, colour = group, linetype = `Number of patients with first prescriptions`)) +
+  geom_line() +
+  facet_wrap(~group, scales = "free") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour=FALSE, linetype = guide_legend(nrow = 2)) +
+  ylab("Total number of patients issued first prescription of \n injectable and depot antipsychotics, per month") +
+  xlab("date") +
+  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+ggsave(filename=here::here("output", "figures", "plot_first_antipsychotics_injectable_and_depot.svg"),
+       antipsychotics_injectable_and_depot,
+       units = "cm", width = 40, height = 20
+)
+
+### Prochlorperazine
+prochlorperazine <-  ggplot(data_processed, aes(x = date, y = prochlorperazine, colour = group, linetype = `Number of patients with first prescriptions`)) +
+  geom_line() +
+  facet_wrap(~group, scales = "free") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(colour=FALSE, linetype = guide_legend(nrow = 2)) +
+  ylab("Total number of patients issued first prescription of \n prochlorperazine antipsychotics, per month") +
+  xlab("") +
+  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+ggsave(filename=here::here("output", "figures", "plot_first_prochlorperazine.svg"),
        prochlorperazine,
        units = "cm", width = 40, height = 20
 )

d19800d8f7229565f81e7d1210c7e6b1c7be2c55
Millie Green <acagreen17@gmail.com>
2021-10-06 11:07:45 +0100

    feat: script for tables 1 and 2

--- analysis/descriptive/table_2.R
+++ analysis/descriptive/table_2.R
@@ -24,22 +24,22 @@ dir.create(here::here("output", "tables"), showWarnings = FALSE, recursive=TRUE)
 source(here("analysis", "lib", "custom_functions.R"))
 
 ## Read in data
-data_cohort <- read_rds(here::here("output", "data", "data_processed.rds"))
+data_cohort <- arrow::read_feather(here::here("output", "data", "input_2021-04-01.feather"))
 
 
 # Table 2 ----
 
 ## All
 table_2_all <- data_cohort %>% 
-  mutate(ethnicity = as.character(ethnicity),
-         ethnicity = ifelse(is.na(ethnicity), "Missing", ethnicity),
+  mutate(ethnicity = as.character(eth2001),
+         ethnicity = ifelse(is.na(eth2001), "Missing", ethnicity),
          ethnicity = fct_case_when(
-           ethnicity == "White" ~ "White",
-           ethnicity == "Mixed" ~ "Mixed",
-           ethnicity == "Asian or Asian British" ~ "Asian or Asian British",
-           ethnicity == "Black or Black British" ~ "Black or Black British",
-           ethnicity == "Other ethnic groups" ~ "Other ethnic groups",
-           ethnicity == "Unknown" ~ "Unknown",
+           ethnicity == "1" ~ "White",
+           ethnicity == "2" ~ "Mixed",
+           ethnicity == "3" ~ "Asian or Asian British",
+           ethnicity == "4" ~ "Black or Black British",
+           ethnicity == "4" ~ "Other ethnic groups",
+           ethnicity == "5" ~ "Unknown",
            ethnicity == "Missing" ~ "Missing",
            #TRUE ~ "Unknown"
            TRUE ~ NA_character_),
@@ -69,14 +69,14 @@ table_2_all <- data_cohort %>%
                        breaks = c(0, 17, 24, 34, 44, 54, 69, 79, Inf),
                        labels = c("0-17", "18-24", "25-34", "35-44", "45-54", "55-69", "70-79", "80+"),
                        right = FALSE)) %>%
-  select(antipsychotic,
+  select(antipsychotic = antipsychotic_any,
          ageband, 
          sex,
          region,
          imd,
          ethnicity) %>%
   tbl_summary(by = antipsychotic) %>%
-  add_overall() 
+  add_overall()
 
 table_2_all <- table_2_all$table_body %>%
   select(group = variable, variable = label, population = stat_0, antipsychotic = stat_2) %>%
@@ -89,14 +89,14 @@ table_2_all <- table_2_all$table_body %>%
   filter(!(is.na(population)),
          !(is.na(antipsychotic))) %>%
   select(-perc, - perc2) %>%
-  mutate(rate = antipsychotic/population*100000)
+  mutate(rate = antipsychotic/population*1000)
 
 ## Sub groups
-table2_autism <- calculate_table2(population = "autism", Y = 10000)
-table2_care_home <- calculate_table2(population = "care_home", Y = 10000)
-table2_dementia <- calculate_table2(population = "dementia", Y = 10000)
-table2_ld <- calculate_table2(population = "learning_disability", Y = 10000)
-table2_smi <- calculate_table2(population = "serious_mental_illness", Y = 10000)
+table2_autism <- calculate_table2(population = "autism", Y = 1000)
+table2_care_home <- calculate_table2(population = "care_home", Y = 1000)
+table2_dementia <- calculate_table2(population = "dementia", Y = 1000)
+table2_ld <- calculate_table2(population = "learning_disability", Y = 1000)
+table2_smi <- calculate_table2(population = "serious_mental_illness", Y = 1000)
 
 
 # Save tables ----

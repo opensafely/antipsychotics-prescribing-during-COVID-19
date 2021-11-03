2e395d6890f2662bd0c8ffc4054981078043b413
Millie Green <acagreen17@gmail.com>
2021-11-03 15:23:08 +0000

    refactor: remove region

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -315,26 +315,6 @@ study = StudyDefinition(
     },
   ),
   
-  ### Region - NHS England 9 regions
-  region = patients.registered_practice_as_of(
-    "index_date",
-    returning = "nuts1_region_name",
-    return_expectations = {
-      "rate": "universal",
-      "category": {
-        "ratios": {
-          "North East": 0.1,
-          "North West": 0.1,
-          "Yorkshire and The Humber": 0.1,
-          "East Midlands": 0.1,
-          "West Midlands": 0.1,
-          "East": 0.1,
-          "London": 0.2,
-          "South West": 0.1,
-          "South East": 0.1,},},
-    },
-  ),
-  
   
   ## Groups
   
